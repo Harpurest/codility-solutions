79bb14886c0a7965ede734985e3e764cf27971f5
Harpurest <vo.duc.hong94@gmail.com>
2025-08-26 08:54:38 +0700

    easy sort

---
+++ src/sorting/MaxProductOfThree.java
@@ -0,0 +1,23 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class MaxProductOfThree {
+    public static void main(String[] args) {
+        MaxProductOfThree maxProductOfThree = new MaxProductOfThree();
+        System.out.println(maxProductOfThree.solution(new int[] {-3 ,1, 2, -2, 5 ,6}));
+    }
+
+    public int solution(int[] A) {
+        Arrays.sort(A);
+        int len = A.length;
+        int max =  A[len-1] * A[len-2] * A[len-3];
+        if (A[0] < 0 && A[1] < 0 && A[len - 1] > 0) {
+            int potential = A[0] * A[1] * A[len - 1];
+            if (potential > max) {
+                max = potential;
+            }
+        }
+        return max;
+    }
+}
