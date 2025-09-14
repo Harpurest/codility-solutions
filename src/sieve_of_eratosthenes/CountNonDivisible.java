2f4b1e70d9e699211a89ad791be7d359b5a43189
Harpurest <vo.duc.hong94@gmail.com>
2025-09-14 15:37:46 +0700

    final solution

--- src/sieve_of_eratosthenes/CountNonDivisible.java
+++ src/sieve_of_eratosthenes/CountNonDivisible.java
@@ -7,11 +7,13 @@ public class CountNonDivisible {
 
     public static void main(String[] args) {
         CountNonDivisible countNonDivisible = new CountNonDivisible();
+
         countNonDivisible.printArray(countNonDivisible.solution(new int[] {3, 1, 2, 3, 6}));
         countNonDivisible.printArray(countNonDivisible.solution(new int[] {2, 4}));
         countNonDivisible.printArray(countNonDivisible.solution(new int[] {1, 2, 4, 8, 16}));
         countNonDivisible.printArray(countNonDivisible.solution(new int[] {3, 9, 27, 36 }));
         countNonDivisible.printArray(countNonDivisible.solution(new int[] {3, 1, 2, 3, 4, 12}));
+        countNonDivisible.printArray(countNonDivisible.solution(new int[] {1, 2, 3, 5, 6, 10, 15, 30}));
     }
 
     private void printArray(int[] A) {
@@ -55,6 +57,7 @@ public class CountNonDivisible {
 
     public int[] solution(int[] A) {
         int len = A.length;
+        int[] results = new int[len];
         int max = A[0];
 
         for (int i = 1; i < len; i++) {
@@ -69,56 +72,23 @@ public class CountNonDivisible {
             tracks[A[i]] += 1;
         }
 
-        int[] divs = new int[max + 1];
-        int i = 2;
-
-        while (i * i <= max) {
-            if (divs[i] == 0) {
-                int k = i * i;
-                while (k <= max) {
-                    if(divs[k] == 0) {
-                        divs[k] = i;
-                    }
-
-                    k += i;
+        int countDiv = 0;
+        long k = 1;
+        for (int i = 0; i < len; i++) {
+            int target = A[i];
+            countDiv = 0;
+            k = 1;
+            while (k * k < target) {
+                if (target % k == 0) {
+                    countDiv += tracks[Math.toIntExact(k)];
+                    countDiv += tracks[Math.toIntExact(target / k)];
                 }
+                k += 1;
             }
-
-            i += 1;
-        }
-
-        int[] results = new int[len];
-        Set<Integer> countSet = new HashSet<>();
-
-        for (int j = 0; j < len; j++) {
-            int target = A[j];
-
-            if (target == 1) {
-                results[j] = len - tracks[target];
-                continue;
+            if (k * k == target) {
+                countDiv += tracks[Math.toIntExact(k)];
             }
-
-            // Fix the case [3, 1, 2, 3, 4, 12] espaccially number 12
-            int trackingReverse = 1;
-
-            while (divs[target] > 0) {
-                countSet.add(target);
-                countSet.add(divs[target]);
-                countSet.add(trackingReverse);
-
-                trackingReverse *= divs[target];
-                target /= divs[target];
-            }
-
-            countSet.add(target);
-            countSet.add(trackingReverse);
-
-            for (int count : countSet) {
-                results[j] += tracks[count];
-            }
-            results[j] = len - results[j];
-
-            countSet.clear();
+            results[i] = len - countDiv;
         }
 
         return results;
